43ea50bdc47dd1d9200ecd0c0e6ecbae62d4a742
naitik-lodha <naitik.lodha139@nmims.edu.in>
2023-10-28 22:41:32 +0530

    Fixed minor bug

--- pages/event/[slug].js
+++ pages/event/[slug].js
@@ -1,23 +1,17 @@
 // [slug].js (Individual Event Page Template)
 import { useRouter } from "next/router";
-import eventData from "@/data/eventData";
 
 const EventPage = () => {
   const router = useRouter();
   const { slug } = router.query;
 
   // Find the event by slug
-  const event = eventData.find((e) => e.slug === slug);
 
-  if (!event) {
-    return <p>Event not found</p>;
-  }
+
 
   return (
     <div className="event-page">
-      <h1>{event.name}</h1>
-      <p>{event.description}</p>
-      {/* Display event gallery, date, and time here */}
+      {slug}
     </div>
   );
 };
