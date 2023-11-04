9717019a4da79c544975fa09422d26cd3f079c0e
Yash <78804783+yashd-dev@users.noreply.github.com>
2023-11-04 18:24:22 +0530

    UI Tweaks

--- components/sections/team.js
+++ components/sections/team.js
@@ -3,79 +3,89 @@ import sanityClient from "@/data/client";
 import { useNextSanityImage } from "next-sanity-image";
 import Image from "next/image";
 
-
 const TeamPage = ({ teamMembers }) => {
-  const [selectedPosition, setSelectedPosition] = useState("All");
-  const uniquePositions = Array.from(
-    new Set(teamMembers.map((member) => member.position))
-  );
+	const [selectedPosition, setSelectedPosition] = useState("All");
+	const uniquePositions = Array.from(
+		new Set(teamMembers.map((member) => member.position))
+	);
 
-  return (
-    <div className="flex flex-col items-center justify-center uppercase" id="team">
-      <h1 className="text-4xl font-extrabold  my-6">
-        Our <span className="text-transparent bg-clip-text bg-gradient-to-tr from-analytikaGreen to-analytikaYellow">Team</span>
-      </h1>
-      <div className="flex flex-wrap gap-4">
-        <div
-          onClick={() => setSelectedPosition("All")}
-          className={`px-4 py-2 text-xl rounded-md hover:cursor-pointer border-analytikaYellow hover:bg-analytikaGreentransition duration-300 ease-in-out ${
-            selectedPosition === "All"
-              ? "underline font-bold bg-clip-text text-transparent bg-gradient-to-tr from-analytikaGreen to-analytikaYellow"
-              : ""
-          }`}
-        >
-          All
-        </div>
+	return (
+		<div
+			className="flex flex-col items-center justify-center uppercase"
+			id="team"
+		>
+			<h1 className="text-4xl font-extrabold  my-6">
+				Our{" "}
+				<span className="text-transparent bg-clip-text bg-gradient-to-tr from-analytikaGreen to-analytikaYellow">
+					Team
+				</span>
+			</h1>
+			<div className="flex flex-wrap gap-4">
+				<div
+					onClick={() => setSelectedPosition("All")}
+					className={`px-4 py-2 text-xl rounded-md hover:cursor-pointer border-analytikaYellow hover:bg-analytikaGreentransition duration-300 ease-in-out ${
+						selectedPosition === "All"
+							? "underline font-bold bg-clip-text text-transparent bg-gradient-to-tr from-analytikaGreen to-analytikaYellow"
+							: ""
+					}`}
+				>
+					All
+				</div>
 
-        {uniquePositions.map((position) => (
-          <div
-            key={position}
-            onClick={() => setSelectedPosition(position)}
-            className={`px-4 py-2 text-xl rounded-md hover:cursor-pointer transition duration-300 ease-in-out ${
-              selectedPosition === position
-                ? "underline font-bold bg-clip-text text-transparent bg-gradient-to-tr from-analytikaGreen to-analytikaYellow"
-                : ""
-            }`}
-          >
-            {position}
-          </div>
-        ))}
-      </div>
-      <ul className="flex flex-wrap justify-center mt-6">
-        {teamMembers.map((member) => {
-          const imageProps = useNextSanityImage(sanityClient, member.image);
+				{uniquePositions.map((position) => (
+					<div
+						key={position}
+						onClick={() => setSelectedPosition(position)}
+						className={`px-4 py-2 text-xl rounded-md hover:cursor-pointer transition duration-300 ease-in-out ${
+							selectedPosition === position
+								? "underline font-bold bg-clip-text text-transparent bg-gradient-to-tr from-analytikaGreen to-analytikaYellow"
+								: ""
+						}`}
+					>
+						{position}
+					</div>
+				))}
+			</div>
+			<ul className="flex flex-wrap justify-center mt-6">
+				{teamMembers.map((member) => {
+					const imageProps = useNextSanityImage(
+						sanityClient,
+						member.image
+					);
 
-          if (
-            selectedPosition === "All" ||
-            member.position === selectedPosition
-          ) {
-            return (
-              <li key={member._id} className="m-4 mx-8 rounded-lg">
-                {member.image && (
-                  <Image
-                    {...imageProps}
-                    className="w-80 h-80" // Set the width to 100% to occupy full width
-                    alt={member.image.alt || ""}
-                  />
-                )}
-                <h2 className="text-xl text-center m-2  text-analytikaWhite mt-4">
-                  {member.name}
-                </h2>
-                <p className="text-lg text-center m-2 text-analytikaGreen mt-2">
-                  {member.role}
-                </p>
-                <p className="text-lg text-center m-2 text-analytikaYellow">
-                  {member.department}
-                </p>
-              </li>
-            );
-          }
-          return null;
-        })}
-      </ul>
-    </div>
-  );
+					if (
+						selectedPosition === "All" ||
+						member.position === selectedPosition
+					) {
+						return (
+							<li
+								key={member._id}
+								className="m-4 mx-8 rounded-lg"
+							>
+								{member.image && (
+									<Image
+										{...imageProps}
+										className="w-[70%] h-auto mx-auto" // Set the width to 100% to occupy full width
+										alt={member.image.alt || ""}
+									/>
+								)}
+								<h2 className="text-xl text-center m-2  text-analytikaWhite mt-4">
+									{member.name}
+								</h2>
+								<p className="text-lg text-center m-2 text-analytikaGreen mt-2">
+									{member.role}
+								</p>
+								<p className="text-lg text-center m-2 text-analytikaYellow">
+									{member.department}
+								</p>
+							</li>
+						);
+					}
+					return null;
+				})}
+			</ul>
+		</div>
+	);
 };
-
 
 export default TeamPage;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { groq } from "next-sanity";
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 import sanityClient from "@/data/client";
 import Navbar from "@/components/Navbar";
 import About from "@/components/sections/about-us";
@@ -9,44 +9,51 @@ import Events from "@/components/sections/events";
 import TeamPage from "@/components/sections/team";
 import ContactUs from "@/components/sections/contact-us";
 
-
 const inter = Inter({ subsets: ["latin"] });
-const myFont = localFont({src:"./coolvetica rg.ttf"});
+const myFont = localFont({ src: "./coolvetica rg.ttf" });
 
-export default function Home({ teamMembers,events }) {
-  return (
-    <main className={`min-h-screen bg-analytikaBlack ${myFont.className} `}>
-      <Navbar />
-      <section className="flex flex-col-reverse mt-8 md:flex-row justify-around">
-        <h1 className="text-6xl font-extrabold mt-12 md:mt-20 ml-10 tracking-wide mb-6 md:mb-0">
-          The only
-          <br />{" "}
-          <span className="font-black text-transparent bg-clip-text bg-gradient-to-tr from-analytikaGreen to-analytikaYellow">
-            Data Science
-          </span>{" "}
-          <br />
-          club of NMIMS
-        </h1>
-        <Image
-          className="md:hover:scale-110"
-          src="Hero-illustration.svg"
-          width={600}
-          height={300}
-          alt="illustration"
-        />
-      </section>
-      <About />
-      <Events events={events}/>
-      <TeamPage teamMembers={teamMembers} />
-      <ContactUs />
-    </main>
-  );
+export default function Home({ teamMembers, events }) {
+	return (
+		<>
+			<Navbar />
+
+			<main
+				className={
+					"min-h-screen bg-analytikaBlack ${myFont.className} p-7 md:max-w-[150ch] mx-auto "
+				}
+			>
+				<section className="flex flex-col-reverse mt-8 md:flex-row justify-between [&>*]:max-w-[150ch] [&>*]:mx-auto text-center  md:text-left">
+					<h1 className="text-5xl md:text-7xl font-extrabold mt-12 md:mt-20 tracking-wide mb-6 md:mb-0">
+						The only
+						<br />{" "}
+						<span className="font-black text-transparent bg-clip-text bg-gradient-to-tr from-analytikaGreen to-analytikaYellow">
+							Data Science
+						</span>{" "}
+						<br />
+						club of NMIMS
+					</h1>
+					<Image
+						className="transition ease-linear delay-100 md:hover:scale-110 min-w-full md:min-w-[50%]"
+						src="Hero-illustration.svg"
+						width={0}
+						height={0}
+						sizes="100vw"
+						alt="illustration"
+					/>
+				</section>
+				<About />
+				<Events events={events} />
+				<TeamPage teamMembers={teamMembers} />
+				<ContactUs />
+			</main>
+		</>
+	);
 }
 export async function getStaticProps() {
-  // Fetch team members from Sanity
-  const query = groq`*[_type == "team"]{_id, name, position, department, role, "image": image.asset->}`;
-  const teamMembers = await sanityClient.fetch(query);
-  const Eventquery = `*[_type == 'events'] {
+	// Fetch team members from Sanity
+	const query = groq`*[_type == "team"]{_id, name, position, department, role, "image": image.asset->}`;
+	const teamMembers = await sanityClient.fetch(query);
+	const Eventquery = `*[_type == 'events'] {
     name,
     description,
     image {
@@ -65,11 +72,11 @@ export async function getStaticProps() {
     }
   }`;
 
-  const events = await sanityClient.fetch(Eventquery);
-  return {
-    props: {
-      teamMembers,
-      events,
-    },
-  };
+	const events = await sanityClient.fetch(Eventquery);
+	return {
+		props: {
+			teamMembers,
+			events,
+		},
+	};
 }
