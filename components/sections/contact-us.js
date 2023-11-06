b95d044a8c2eb8bd5344d967556e1acc245a00ce
naitik-lodha <naitik.lodha139@nmims.edu.in>
2023-11-06 15:35:45 +0530

    Final fomr

--- components/sections/contact-us.js
+++ components/sections/contact-us.js
@@ -1,4 +1,40 @@
+import { useRef } from "react";
 export default function Contact() {
+	const nameRef = useRef(null);
+	const emailRef = useRef(null);
+	const messageRef = useRef(null);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+const url=process.env.NEXT_PUBLIC_APPSCRIPT_URL;
+		const data = {
+		  name: nameRef.current?.value,
+		  email: emailRef.current?.value,
+		  message: messageRef.current?.value,
+		};
+	
+		const requestOptions = {
+		  method: "POST",
+		  headers: { "Content-Type": "application/json" },
+		  body: JSON.stringify(data),
+		  mode: "no-cors",
+		};
+	
+		fetch(url, requestOptions)
+		  .then((response) => {
+			console.log(response);
+			alert("Message sent sucessfully!");
+			nameRef.current.value = "";
+			emailRef.current.value = "";
+			messageRef.current.value = "";
+		  })
+	
+		  .catch((error) => {
+			console.error("Error:", error);
+			// Handle any errors that occurred during the request
+		  });
+	  };
+
   return (
     <section className="md:flex md:justify-center mt-16 pt-20" id="contact-us">
       <div className="max-w-md mx-6 md:max-w-xl md:mx-0 md:mr-16">
@@ -23,19 +59,22 @@ export default function Contact() {
         </div>
       </div>
       <div className="max-w-md mx-6 md:mx-2 mt-6 md:w-1/2 md:mt-0">
-        <form className="text-md flex flex-col" netlify>
+        <form className="text-md flex flex-col" onSubmit={handleSubmit}>
           <input
             type="text"
+			ref={nameRef}
             placeholder="Name: John Doe"
             className="border-gray-200 bg-transparent border-2 rounded-md p-3 mb-4 transition duration-300"
           />
           <input
             type="email"
+			ref={emailRef}
             placeholder="Email: xyz@example.com"
             className="border-gray-200 bg-transparent border-2 rounded-md p-3 mb-4 transition duration-300"
           />
           <textarea
             placeholder="Type your heart out!"
+			ref={messageRef}
             rows="4"
             className="border-gray-200 bg-transparent border-2 rounded-md p-3 mb-4 transition duration-300"
           ></textarea>
--- data/client.js
+++ data/client.js
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client";
 
 export default createClient({
-  projectId: "gntxbl4n",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: "production",
   useCdn: true,
   apiVersion: "2023-11-03",
