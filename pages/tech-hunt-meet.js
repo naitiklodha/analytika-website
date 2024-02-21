fe6473812536ead9376da6e4f6d8715bd8a2bdaf
Naitik Lodha <69336744+naitiklodha@users.noreply.github.com>
2024-02-21 17:09:54 +0530

    Update tech-hunt-meet.js

---
+++ pages/tech-hunt-meet.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function ReadMore() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("https://meet.google.com/tzk-mmvi-ovg")
+      .then(() => {})
+      .catch((error) => {});
+  }, []);
+
+  return (
+    <div>
+      Redirecting.....
+    </div>
+  );
