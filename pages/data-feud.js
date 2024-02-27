5e28b42e1651ce5ffa91b6c0f7c72445b67b4b7e
Naitik Lodha <69336744+naitiklodha@users.noreply.github.com>
2024-02-27 13:23:11 +0530

    Update data-feud.js

--- pages/data-feud.js
+++ pages/data-feud.js
@@ -5,7 +5,7 @@ export default function ReadMore() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("https://docs.google.com/forms/d/e/1FAIpQLSerGuP4er0P_YeWtde_EN8miZNrImD6VnxJlxSjGhReJ6ok1g/viewform")
+    router.push("https://forms.gle/QixKdk8CUGJs2r7X7")
       .then(() => {})
       .catch((error) => {});
   }, []);
