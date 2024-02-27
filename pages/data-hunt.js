8052c275a4fa94b2f4addaef43fe26941b54e9e9
Naitik Lodha <69336744+naitiklodha@users.noreply.github.com>
2024-02-27 12:18:19 +0530

    Added new routes

--- pages/data-hunt.js
+++ pages/data-hunt.js
@@ -5,7 +5,7 @@ export default function ReadMore() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push(https://forms.gle/2B2y45rUZzdjkdiS6")
+    router.push("https://forms.gle/2B2y45rUZzdjkdiS6")
       .then(() => {})
       .catch((error) => {});
   }, []);
