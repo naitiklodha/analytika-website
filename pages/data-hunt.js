47a071da0266a3f9f4bab1462b8a3fe05184be3a
Naitik Lodha <69336744+naitiklodha@users.noreply.github.com>
2024-02-27 12:13:58 +0530

    Create data-hunt.js

---
+++ pages/data-hunt.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function ReadMore() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(https://forms.gle/2B2y45rUZzdjkdiS6")
+      .then(() => {})
+      .catch((error) => {});
+  }, []);
+
+  return (
+    <div>
+      Redirecting.....
+    </div>
+  );
+}
